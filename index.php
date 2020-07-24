71b6737c7c475e358e7709d37167c19e871c3f62
Aaditya55 <64467870+Aaditya55@users.noreply.github.com>
2020-07-24 16:08:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content = "This website provides you with best deals and offers. The paradise on earth. Get anything you wish for in affordable price.">
+	<title>SocialDistancing.com/login/user</title>
+	<link rel="stylesheet" type="text/css" href="a1.css">
+</head>
+<body>
+	<div class="login-box">
+		<h1>Login</h1>
+		 <form action="new1.php" method="POST">
+		  <label for="username"><b>username</b></label>
+          <input type="text" name="username" required>
+   
+          <label for="password"><b>password</b></label>
+          <input type="text" name="password" required>
+    	   <a href="site.html">
+           <input class="btn" type="button" name="check" value="Log in"></a>
+
+         </form>
+		<a href="pro2.php">Register</a>
+		<br/>
+		<br/>
+		<a href="reset.html" >Forgot Password ?</a>
+	</div>
+</body>
+</html>
